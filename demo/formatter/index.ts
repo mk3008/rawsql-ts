4a162caca46aa9bb15d8fad9926a32c0eb4f5a67
MSugiura <mk3008.develop@gmail.com>
2025-04-20 10:20:04 +0900

    Add comments to indicate expected console output in `demo/formatter/index.ts`
    * Add a comment to indicate the expected formatted SQL output after logging it to the console

--- demo/formatter/index.ts
+++ demo/formatter/index.ts
@@ -4,4 +4,7 @@ const sql = `SELECT id, name FROM users WHERE active = TRUE`;
 const query = SelectQueryParser.parse(sql);
 const formatter = new Formatter();
 const formattedSql = formatter.format(query);
-console.log(formattedSql);
+
+console.log(formattedSql); 
+
+// => select "id", "name" from "users" where "active" = true
